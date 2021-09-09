52620a3bfdffa5d003da931f4815c3a85dc068d3
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2021-09-09 10:23:40 -0300

    Incluindo ManyToMany

--- src/main/java/teste/ObterPedido.java
+++ src/main/java/teste/ObterPedido.java
@@ -1,5 +1,7 @@
 package teste;
 
+import java.util.List;
+
 import infra.DAO;
 import onetoone.ItemPedido;
 import onetoone.Pedido;
@@ -10,6 +12,7 @@ public class ObterPedido {
 		DAO<Pedido> dao = new DAO<>(Pedido.class);
 		
 		Pedido pedido = dao.obterPorId(3L);
+		List<Pedido> pedidoss = dao.obterTodos(10, 4);
 		
 		for (ItemPedido pedidos : pedido.getItens()) {
 			System.out.println(pedidos.getProduto().getNome());
@@ -17,6 +20,7 @@ public class ObterPedido {
 			
 		}
 		
+		pedidoss.stream().map(p -> p.getData()).forEach(System.out::println);
 		dao.fechar();
 	}
 
